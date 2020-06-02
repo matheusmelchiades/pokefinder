2ce52ed5f09807e0570de7a38fde8eee0ccb00a3
matheusmelchiades <matheusmacielmelchiades@hotmail.com>
2020-06-02 09:55:04 -0300

    implemented user form page

--- src/pages/UserForm/index.js
+++ src/pages/UserForm/index.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useEffect, createRef } from 'react';
+
+import Icon from '../../components/Icon';
+import Input from '../../components/Inputs/Standard';
+
+import { Container, Header, Content, Footer, Title, Info, IconButton } from './styles';
 
 export default function UserForm({ history }) {
-  return <div>USER FORM</div>;
+  const inputRef = createRef();
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [inputRef]);
+
+  return (
+    <Container>
+      <Header>
+        <Title>Let's meet each other first?</Title>
+      </Header>
+      <Content>
+        <Info>First we need to know your name...</Info>
+        <Input ref={inputRef} />
+      </Content>
+      <Footer>
+        <IconButton onClick={() => history.push('/form/perfil')}>
+          <Icon name="next" size="large" />
+        </IconButton>
+      </Footer>
+    </Container>
+  );
 }
---
+++ src/pages/UserForm/styles.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+import devices from '../../styles/devices';
+
+import BackgroundScreen from '../../assets/background/bg.png';
+import BackgroundPlus from '../../assets/background/Plus.png';
+
+import PokemonLogoImage from '../../assets/pokemon-logo/pokemon-logo@2x.png';
+import FinderLogoImage from '../../assets/finder/finder@2x.png';
+import PikachuImage from '../../assets/pikachu/pikachu@3x.png';
+
+export const Container = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  background: url(${BackgroundScreen});
+
+  @media ${devices.desktop} {
+    background: url(${BackgroundPlus});
+  }
+`;
+
+export const Header = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  padding-top: 17%;
+  padding-left: 10%;
+  padding-right: 10%;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex: 2;
+  flex-direction: column;
+  justify-content: flex-start;
+
+  padding-top: 10%;
+  padding-left: 10%;
+  padding-right: 10%;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+
+export const Title = styled.p`
+  font-weight: 500;
+  font-size: 24px;
+`;
+
+export const Info = styled.p`
+  font-weight: 400;
+  font-size: 22px;
+  margin-bottom: 20px;
+`;
+
+export const IconButton = styled.div`
+  :active {
+    transform: translate(-2px, 4px);
+  }
+`;
