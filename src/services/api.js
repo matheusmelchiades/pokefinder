434e6a94e199a4f41385b6ff56f86a522beff8c7
matheusmelchiades <matheusmacielmelchiades@hotmail.com>
2020-06-03 10:51:35 -0300

    change header api

--- src/services/api.js
+++ src/services/api.js
@@ -2,4 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: process.env.REACT_APP_API || 'http://localhost:5000',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Accept: 'application/json',
+  },
 });
